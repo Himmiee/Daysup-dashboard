567a364b5e13d1051bbdf9b358f48762c78b5e80
Haliyah Tijani <haliyya460@gmail.com>
2023-07-16 22:19:25 +0100

    deploy fix

--- Server/index.ts
+++ Server/index.ts
@@ -6,32 +6,34 @@ import compression from "compression";
 import dotenv from "dotenv";
 import cors from "cors";
 import mongoose from "mongoose";
-import router from "./router"
-import { emailTemplate } from "./helpers/template";
+import router from "./src/router";
+import { emailTemplate } from "helpers/template";
 
 const app = express();
 
-dotenv.config()
+dotenv.config();
 
 const port = process.env.PORT || "3050";
 
-const URI = process.env.DB_CONNECT
-mongoose.connect(URI)
+const URI = process.env.DB_CONNECT;
+mongoose.connect(URI);
 
-app.use(compression())
-app.use(cookieParser())
+app.use(compression());
+app.use(cookieParser());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(cors({
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  cors({
     // origin: 'http://localhost:3000',
-    origin: '*',
-}))
+    origin: "*",
+  })
+);
 
 // emailTemplate()
 const server = http.createServer(app);
 
 server.listen(port, () => {
-    console.log("app is listening on port " + port);
+  console.log("app is listening on port " + port);
 });
 
-app.use("/", router())
+app.use("/", router());
