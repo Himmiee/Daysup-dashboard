64e6106060ea6ed631e9c2d87ec4af09c89db317
Haliyah Tijani <haliyya460@gmail.com>
2023-07-16 22:45:34 +0100

    deploy fix

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -6,7 +6,7 @@ import compression from "compression";
 import dotenv from "dotenv";
 import cors from "cors";
 import mongoose from "mongoose";
-import router from "./src/router";
+import router from "./router";
 import { emailTemplate } from "helpers/template";
 
 const app = express();
